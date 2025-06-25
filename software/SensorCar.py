2fbeef8821e392d638de62e80b6ebb4eebd473d5
c2c-Christian <info@ohp24.de>
2025-06-25 15:42:44 +0200

    Fahrmodus 7 hinzugefügt.

--- software/SensorCar.py
+++ software/SensorCar.py
@@ -12,80 +12,17 @@ class SensorCar(BaseCar):
         self.distance = 0
         self.INf = bk.Infrared(INf_offset)
         self.fahrdaten = []
-    def fahrmodus5(self): #Funktion für Fahrmodus5.
-        doku = "nein"
-        zaehler = 0 #Wird benötigt wenn Fahrzeug zwischen dem Sensor steht --> Weiterfahrt zwischen IR-Sensor.
-        while True:
-            #print(self.INf.read_digital())
-            print(self.INf.read_analog())
-            print(self.INf.read_digital())
-            print(self.USo.distance())
-            #time.sleep(1)
 
-            INf_daten = self.INf.read_digital() # Liste mit IR-Werte Sensor 1-5
-            if INf_daten[2] == 1: # 2 = Sensor in der Mitte --> Abfrage belegt.
-                if self.steering_angle != 90:
-                    doku = "ja"
-                self.steering_angle = 90
-                self.frontwheels.turn(self.steering_angle)
-                self.speed = 30
-                self.drive()
-                zaehler = 0
-            else:# Lenkwinkelkorrektur wenn Sensor in der Mitte nicht belegt. 0 = Sensor in Fahrtrichtung Links aussen 1,2,..., 4 = Sensor in Fahrtrichtung rechts aussen.
-                if INf_daten[1] == 1:
-                    if self.steering_angle != 70:
-                        doku = "ja" 
-                    self.steering_angle = 70
-                    self.frontwheels.turn(self.steering_angle)
-                    zaehler = 0
-                elif INf_daten[3] == 1:
-                    if self.steering_angle != 110:
-                        doku = "ja"
-                    self.steering_angle = 110
-                    self.frontwheels.turn(self.steering_angle)
-                    zaehler = 0
-                elif INf_daten[0] == 1:
-                    if self.steering_angle != 45:
-                        doku = "ja"
-                    self.steering_angle = 45
-                    self.frontwheels.turn(self.steering_angle)
-                    zaehler = 0
-                elif INf_daten[4] == 1:
-                    if self.steering_angle != 135:
-                        doku = "ja"
-                    self.steering_angle = 135
-                    self.frontwheels.turn(self.steering_angle)
-                    zaehler = 0         
-                else: # Stopp bei Fahrbahnende wenn 10 Schleifen keiner der 5 Sensor belegt wird.
-                    zaehler += 1
-                    if zaehler == 20:
-                        self.stop()
-                        break
-                if doku=='ja':
-                    liste = self.INf.read_digital()
-                    for i in range(len(liste)):
-                        liste[i] = int(liste[i])
-                    print(liste)
-                    print(type(liste))
-                    print(type(liste[0]))
-                    try:
-                        
-                        self.fahrdaten.append({"Fahrmodus": 5, "Zeit": time.time(), "Richtung": self.direction,"Geschwindigkeit": self.speed, "Lenkwinkel": self.steering_angle,"Entfernung": self.USo.distance(), "IR-Sensor": liste})
-                        
-                    except Exception as e:
-                        print(f"Konnte nicht gespeichert werden: {e}")
-                    doku = 'nein'
-                    print(self.fahrdaten)
-        speichern = save_fahrdaten(self.fahrdaten)   
-        speichern.save()
-        return "Fahrmodus 5 beendet"
-
-    def fahrmodus6(self): #Funktion für Fahrmodus 6.
+    def fahrmodus(self, funktionserweiterung=0): #Funktion für Fahrmodus, Funktionserweiterung=1 --> für "scharfe" Kurven mit rücksetzen. Funktionserweiterung=2 --> "scharfe" Kurven + integration Ultraschallsensor.
         sensor = 99
         drive_korrektur = 99
         doku = "nein"
         zaehler = 0 #Wird benötigt wenn Fahrzeug zwischen dem Sensor steht --> Weiterfahrt zwischen IR-Sensor.
         while True:
+            if funktionserweiterung == 2:
+                if 0< self.USo.distance() < 5:
+                    self.stop()
+                    break
             #print(self.INf.read_digital())
             print(self.INf.read_analog())
             print(self.INf.read_digital())
@@ -131,7 +68,7 @@ class SensorCar(BaseCar):
                     self.steering_angle = 135
                     self.frontwheels.turn(self.steering_angle)
                     zaehler = 0         
-                else: # Stopp bei Fahrbahnende wenn 10 Schleifen keiner der 5 Sensor belegt wird.
+                else: # Stopp bei Fahrbahnende wenn 20 Schleifen keiner der 5 Sensor belegt wird.
                     zaehler += 1
                     if zaehler == 10:
                         if sensor == 0 or sensor == 1:
@@ -144,34 +81,35 @@ class SensorCar(BaseCar):
                             self.stop()
                             break
                         else:
-                            self.stop()
-                            while True:
-                                INf_daten = self.INf.read_digital()
-                                if sensor == 0 or sensor == 1:
-                                    drive_korrektur = 4
-                                    print("sensor 0 und 1")
-                                    self.steering_angle = 135
-                                if sensor == 3 or sensor == 4:
-                                    drive_korrektur = 0
-                                    print("sensor 3 und 4")
-                                    self.steering_angle = 45
-                                self.frontwheels.turn(self.steering_angle)
-                                self.speed = -30
-                                print(sensor)
-                                print(INf_daten)
-                                self.drive()
-                                if INf_daten[drive_korrektur] == 1:
-                                    self.speed = 0
-                                    self.stop()
+                            if funktionserweiterung > 0:
+                                self.stop()
+                                while True:
+                                    INf_daten = self.INf.read_digital()
                                     if sensor == 0 or sensor == 1:
+                                        drive_korrektur = 4
                                         print("sensor 0 und 1")
-                                        self.steering_angle = 45
-                                    if sensor == 3 or sensor == 4:
-                                        print("sensor 3 und 4")
                                         self.steering_angle = 135
-                                    self.speed = 30
+                                    if sensor == 3 or sensor == 4:
+                                        drive_korrektur = 0
+                                        print("sensor 3 und 4")
+                                        self.steering_angle = 45
+                                    self.frontwheels.turn(self.steering_angle)
+                                    self.speed = -30
+                                    print(sensor)
+                                    print(INf_daten)
                                     self.drive()
-                                    break
+                                    if INf_daten[drive_korrektur] == 1:
+                                        self.speed = 0
+                                        self.stop()
+                                        if sensor == 0 or sensor == 1:
+                                            print("sensor 0 und 1")
+                                            self.steering_angle = 45
+                                        if sensor == 3 or sensor == 4:
+                                            print("sensor 3 und 4")
+                                            self.steering_angle = 135
+                                        self.speed = 30
+                                        self.drive()
+                                        break
                 if doku=='ja':
                     liste = self.INf.read_digital()
                     for i in range(len(liste)):
@@ -189,8 +127,15 @@ class SensorCar(BaseCar):
                     print(self.fahrdaten)
         speichern = save_fahrdaten(self.fahrdaten)   
         speichern.save()
+    def fahrmodus5(self): #Funktion für Fahrmodus 5.
+        self.fahrmodus()
+        return "Fahrmodus 5 beendet"
+    def fahrmodus6(self): #Funktion für Fahrmodus 6.
+        self.fahrmodus(1)
         return "Fahrmodus 6 beendet"
-
+    def fahrmodus7(self): #Funktion für Fahrmodus 7.
+        self.fahrmodus(2)
+        return "Fahrmodus 7 beendet"
     def kalibrieren(self):
         while True:
             print(self.INf.read_analog())
@@ -198,5 +143,5 @@ class SensorCar(BaseCar):
             print(self.USo.distance())
             time.sleep(1)
 car = SensorCar(0,0,0,[60,70,80,60,40])  
-car.fahrmodus6()
+car.fahrmodus7()
 #car.kalibrieren()
